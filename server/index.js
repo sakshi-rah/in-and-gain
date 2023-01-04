dd688d4070e082fc0bf4d93a568f17e70a234d5c
sakshi rahangdale <rahangdalesakshi2@gmail.com>
2023-01-04 22:29:25 +0530

    modify port

--- server/index.js
+++ server/index.js
@@ -5,10 +5,13 @@ dotenv.config()
 
 const app = express();
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGODB_URL,()=>{
     console.log('connected to mongoose')
 })
 
-app.listen(5000,()=>{
-    console.log('server started running on PORT 5000')
+app.listen(PORT,()=>{
+    console.log(`server started running on PORT ${PORT}`)
 })
