87e876244df5fef8a3584fd1e2c99052a4c5f38f
sakshi rahangdale <rahangdalesakshi2@gmail.com>
2023-01-17 22:25:45 +0530

    css apply on signup

--- client/src/views/Signup/Singup.js
+++ client/src/views/Signup/Singup.js
@@ -1,39 +1,61 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import axios from 'axios'
 import "./Signup.css"
 import signupImg from "./../../Images/signup-page-pic.png"
+import profilePic from "./../../Images/profile-pic.png"
+
 
 function Singup() {
+    const [name, setName] =useState("")
+    const [email, setEmail] =useState("")
+    const [phone, setPhone] =useState("")
+    const [password, setPassword] =useState("")
+    const [role, setRole] =useState("user")
+     
+    async function signupUser(){
+        const response = await axios.post('/signup', {
+            name:name,
+            email:email,
+            password:password,
+            phone:phone,
+            role:role
+        })
+        console.log(response.data)
+    }
     return (
         <div>
-            <h1 className='text-center'>create Account</h1>
+            <h1 className='text-center mt-3 signup-heading'>create Account</h1>
             <div className='row'>
                 <div className='col-md-6'>
                     <div className='form-container '>
                         <form>
                             <div className='first-container'>
+                                <div className='profile-pic-container'>
+                                    <img src={profilePic} className='profile-pic'/>
+                                </div>
+                                <div className='form-item'>
+                                    <label htmlFor='name' className=''>Full Name  </label>  
+                                    <input type="text" id='name' className='form-input' value={name} onChange = {(e)=>setName(e.target.value)}/>
+                                </div>
+
 
                                 <div className='form-item'>
-                                    <label htmlFor='name' className=''>First Name  </label>  {<br></br>}
-                                    <input type="text" id='name' className='form-input' />
+                                    <label htmlFor='email'>Email </label> {<br></br>}
+                                    <input type="email" id='suremailname' className='form-input'value={email} onChange = {(e)=>setEmail(e.target.value)} />
                                 </div>
 
                                 <div className='form-item'>
-                                    <label htmlFor='surname'>Last Name  </label>{<br></br>}
-                                    <input type="text" id='surname' className='form-input' />
+                                    <label htmlFor='password'>Password </label>
+                                    <input type="password" id='password' className='form-input' value={password} onChange = {(e)=>setPassword(e.target.value)}/>
                                 </div>
 
                                 <div className='form-item'>
-                                    <label htmlFor='email'>Email  </label>{<br></br>}
-                                    <input type="email" id='email' className='form-input' />
-                                </div>
-
-                                <div className='form-item'>
-                                    <label htmlFor='password'>Password </label>{<br></br>}
-                                    <input type="password" id='password' className='form-input' />
+                                    <label htmlFor='phone'>Phone No. </label>
+                                    <input type="text" id='phone' className='form-input'value={phone} onChange = {(e)=>setPhone(e.target.value)} />
                                 </div>
 
                                 <div className='btn-box'>
-                                <button type='submit' className='create-btn'> Create</button>
+                                <button type='button' className='create-btn' onClick={signupUser}> Create</button>
                                 </div>
 
                             </div>
