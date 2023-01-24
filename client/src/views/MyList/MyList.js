2c81cb02ecad990d878a107a498a4e43fcb39a84
sakshi rahangdale <rahangdalesakshi2@gmail.com>
2023-01-24 09:59:49 +0530

    added placefoodlistitem function

--- client/src/views/MyList/MyList.js
+++ client/src/views/MyList/MyList.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import './MyList.css'
+import axios from "axios"
 import Navbar from '../../components/Navbar/Navbar'
 
-import {myFoodListItems} from '../../util/myList'
+import { myFoodListItems } from '../../util/myList'
+import { currentUser } from '../../util/currentUser'
 
 function MyList() {
+ async function placeFoodListItem(){
+  
+ }
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <h1 className='text-center'>MyList</h1>
       {
-        myFoodListItems.map((item, index)=>{
+        myFoodListItems.map((item, index) => {
           return (
-          <div>
-            <h6>Name: {item.name}</h6>
-            <h6>Quantity: {item.quantity}</h6>
-            <h6>Price: {item.price}</h6>
-            <hr/>
-          </div>)
+            <div>
+              <h6>Name: {item.name}</h6>
+              <h6>Quantity: {item.quantity}</h6>
+              <h6>Price: {item.price}</h6>
+              <hr />
+            </div>)
         })
       }
       <button className='btn btn-primary'>Confirm Order</button>
