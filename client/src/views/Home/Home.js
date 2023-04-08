f072dc9fa87f8d492500f42083c82bea1bdb6fe2
sakshi-rah <rahangdalesakshi2@gmail.com>
2023-04-08 12:47:47 +0530

    ref: modified landing page

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -20,32 +20,35 @@ function Home() {
     <div >
       <Navbar user={currentUser?.name} />
 
-      <div className='row'>
-        <div className='col-md-6 '>
+      <div className='row mt-3'>
 
-          <div className='sweethub-container'>
-            <p><img src={shLogo} className='home-shLogo ' /> SweetHub</p>
+        <div className='mt-5 d-flex '>
+
+          <div className='col-md-6 col-sm-12 col-12 left-container'>
+
+            <div className='sweethub-container'>
+              <p><img src={shLogo} className='home-shLogo ' /> SweetHub</p>
+            </div>
+
+            <div className='home-page-container'>
+
+              <span className='home-page-para'>Healthy & delicious sweet taste</span>
+
+              <p>These healthy desserts cover all the best sweet trests. providing fresh and <br />
+                sweet teste desserts,you can serve and enjoy with dessert stay happy!
+              </p>
+
+              <Link to='/myOrders'><button className='btn btn-danger' >Order Now</button></Link>
+              <Link to='/bookTable'><button className='btn btn-outline-danger ms-5 '>Book Table</button></Link>
+
+            </div>
+
           </div>
 
-          <div className='home-page-container'>
-
-            <span className='home-page-para'>Healthy & delicious sweet taste</span>
-
-            <p>These healthy desserts cover all the best sweet trests. providing fresh and <br />
-              sweet teste desserts,you can serve and enjoy with dessert stay happy!
-            </p>
-
-            <Link to='/myOrders'><button className='btn btn-danger' >Order Now</button></Link>
-            <Link to='/bookTable'><button className='btn btn-outline-danger ms-5 '>Book Table</button></Link>
-
+          <div className='col-md-6 col-sm-12 col-12 right-container'>
+            <img src={mithaiImg} className="mithaiimg" />
           </div>
-
         </div>
-
-        <div className='col-md-6 '>
-          <img src={mithaiImg} className="mithaiimg" />
-        </div>
-        
       </div>
 
     </div>
